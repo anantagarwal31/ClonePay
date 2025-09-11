2d2302f45b94c81cce7f57cc9f1f6d39d8516103
Anant Agarwal <anantagarwal2003@gmail.com>
2025-09-11 23:24:34 +0530

    Added user signup route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,69 @@
 const express = require("express");
 const userRouter = express.Router();
+const { z } = require("zod")
+const bcrypt = require("bcrypt")
+const jwt = require("jsonwebtoken")
+const { JWT_SECRET } = require("../config");
+const { User } = require("../db");
 
-userRouter.post("/signup",()=>{
-    
+userRouter.post("/signup",async (req,res)=>{
+    const reqBody = z.object({
+        firstName: z.string(),
+        lastName: z.string(),
+        username: z.string(),
+        password: z.string().min(6).max(30).refine((value)=>/[A-Z]/.test(value)).refine((value)=>/[a-z]/.test(value)).refine((value) => /[0-9]/.test(value)).refine((value)=>/[\W_]/.test(value))
+    })
+
+    const parsedReqBody = reqBody.safeParseAsync(req.body);
+    if(!parsedReqBody.success){
+        res.json({
+            message:"Incorrect format",
+            error: parsedReqBody.error
+        });
+        return
+    }
+
+    const {firstName, lastName, username, password} = req.body;
+    const user = User.findOne({
+        username: username
+    })
+
+    if(user){
+        res.json({
+            message:"User already exists"
+        });
+        return
+    }
+
+    let errorThrown = false;
+    try{
+        const hashedPass = await bcrypt.hash(password,5);
+
+        const user = await User.create({
+            firstName: firstName,
+            lastName: lastName,
+            username: username,
+            password: hashedPass
+        });
+
+        const userId = user._id;
+
+        const token = jwt.sign({
+            userId
+        },JWT_SECRET);
+    }
+    catch(e){
+        res.json({
+            error
+        });
+        errorThrown = true;
+    }
+
+    if(!errorThrown){
+        res.json({
+            message:"You have signed up successfully"
+        })
+    }
 })
 
 module.exports = userRouter
