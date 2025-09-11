39209428171c7833d8672f53d30195fb81a487de
Anant Agarwal <anantagarwal2003@gmail.com>
2025-09-11 22:46:29 +0530

    Added main and user Router

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,22 @@
+require("dotenv").config()
 const express = require("express");
+const mongoose = require("mongoose")
+const app = express();
+const router = require("./routes/index")
+app.use("/api/v1", router)
 
+async function main(){
+    try{
+        await mongoose.connect(process.env.MONGO_URL);
+        console.log("Connected to MongoDB successfully");
 
+        app.listen(process.send.PORT,()=>{
+            console.log(`Server running on port ${process.env.PORT}`)
+        })
+    }
+    catch(e){
+        console.log("Error while connecting to MongoDB", e);
+    }
+}
+
+main();
