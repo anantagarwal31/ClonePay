8e618f998b488dac14ba976f1a7ee46590755844
Anant Agarwal <anantagarwal2003@gmail.com>
2025-09-17 18:48:21 +0530

    Added search by name route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -128,4 +128,29 @@ userRouter.put('/update', authMiddleware, async (req,res)=>{
     }
 })
 
+userRouter.get('/bulk', async (req,res)=>{
+    const filter = req.query.filter || "";
+
+    const users = await User.find({
+        $or:[{
+            firstName:{
+                "$regex":filter
+            }
+        },{
+            lastName:{
+                "$regex":filter
+            }
+        }]
+    })
+
+    res.json({
+        user:users.map(user=>({
+            username:user.username,
+            firstName:user.firstName,
+            lastName:user.lastName,
+            _id:user._id
+        }))
+    })
+})
+
 module.exports = userRouter
