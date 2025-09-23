b896bf3273d45bc30c4c4769e90bd1121d3afb76
Anant Agarwal <anantagarwal2003@gmail.com>
2025-09-23 21:31:47 +0530

    Minor fix

--- backend/middleware.js
+++ backend/middleware.js
@@ -3,7 +3,6 @@ const jwt  = require("jsonwebtoken");
 
 const authMiddleware = (req,res,next)=>{
     const authHeader = req.get('Authorization');
-    console.log("Auth header:", authHeader);
 
     if(!authHeader || !authHeader.startsWith('Bearer ')){
         return res.status(403).json({});
